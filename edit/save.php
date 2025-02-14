ef6996882adfff1004818c707c0934ec399d7071
Xebilon <128182636+Xebilon@users.noreply.github.com>
2025-02-14 07:48:43 +0100

    Delete edit directory

--- edit/save.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-    file_put_contents('index.html', $data['content']);
-    echo json_encode(['status' => 'success']);
-}
-?>
